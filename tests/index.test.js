32d838e57448c57e0da791b3495d589639e449be
Ikechi Michael <mykehell123@gmail.com>
2018-01-19 14:07:34 +0100

    test: create tests

---
+++ tests/index.test.js
@@ -0,0 +1,21 @@
+require('../src/index')
+
+const { assert } = require('chai')
+
+describe('String.compare', () => {
+    it('should return 0 if both strings are empty', () => {
+        assert.equal(String.compare('', ''), 0)
+    })
+
+    it('should return 0 if one of strings is empty', () => {
+        assert.equal(String.compare('hello', ''), 0)
+    })
+
+    it('should return 1 if strings are non-empty and same', () => {
+        assert.equal(String.compare('hello', 'hello'), 1)
+    })
+    
+    it('should be > 0.5 if strings are non-empty and second string is found in first', () => {
+        assert.isTrue(String.compare('hello', 'he') > 0.5)
+    })
+})
